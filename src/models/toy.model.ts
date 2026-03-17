cfa6518826c319f71e210ffe01e425cca7811567
Милан Павлоивћ <mpavlovic781@gmail.com>
2026-03-17 15:50:31 +0100

    Initial commit

---
+++ src/models/toy.model.ts
@@ -0,0 +1,23 @@
+export interface ToysModel{
+    
+    toyId: number
+    name: string
+    permalink: string
+    description: string
+    targetGroup: string
+    productionDate: string
+    price: number
+    imageUrl: string
+
+    ageGroup: {
+        ageGroupId: number
+        name: string
+        description: string
+    }
+    
+    type: {
+        typeId: number
+        name: string
+        description: string
+    }
+}
